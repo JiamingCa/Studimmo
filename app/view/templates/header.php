c4b0077a40693e24887e841e285a4fefa8cc698d
Samysamy57 <152170198+Samysamy57@users.noreply.github.com>
2024-12-06 14:29:08 +0100

    Doc MVC et les templates

---
+++ app/view/templates/header.php
@@ -0,0 +1,22 @@
+<header>
+    <a href="homepage.html" class="logo-link">
+        <img src="../asset/image/Logo_studimo.png" alt="Logo de STUDIMMO" class="logo">
+    </a>
+    <nav class="nav-menu">
+        <a href="trouver-logement.html" class="nav-link">Trouver un logement</a>
+        <a href="publier-annonce.html" class="nav-link">Publier une annonce</a>
+        <a href="faq.html" class="nav-link">FAQ</a>
+    </nav>
+    
+    <a href="inscription.html" class="inscription-button" id="inscriptionButton">S’inscrire</a>
+    <div class="profile-avatar" id="profileAvatar" style="display: none;">
+        <img src="../asset/image/default_avatar.png" alt="Avatar de profil" class="avatar-img">
+        <div class="profile-icons" id="profileIcons" style="display: flex;">
+            <i href="favoris.php" class="bi bi-heart"></i>
+            <i href="message.php" class="bi bi-chat"></i>
+            <i href="notification.php" class="bi bi-bell"></i>
+        </div>
+    </div>
+    <div class="burger-menu">&#9776;</div>
+</header>
+        <script src="../asset/js/header.js"></script>
