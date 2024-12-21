1d56d9a03be0ff62885654562a81466c6ea1a3dd
Samysamy57 <152170198+Samysamy57@users.noreply.github.com>
2024-12-21 17:27:21 +0100

    .

--- app/controllers/AnnonceController.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/controllers/MonEspaceController.php
-class AnnonceController {
-    public function afficherAnnonce() {
-       
-        // Inclure la vue
-        require 'app/view/pages/annonce.php';
-    }
-}
--- app/controllers/GererUtilisateurController.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require 'app/view/pages/GererUtilisateur.php';
-
-?>
